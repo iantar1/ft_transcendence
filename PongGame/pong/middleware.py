41bba0298d401ee4b68ac37d9c4a2ade863e350a
Your Name <you@example.com>
2024-12-16 04:28:50 +0100

    adding game app success and we merge everything

--- PongGame/pong/middleware.py
+++
@@ -1,9 +0,0 @@
-def new_middleware(get_response):
-
-    def middleware(request):
-        print('before')
-        response = get_response(request)
-        print(f"response: {response}")
-        return response
-
-    return middleware
